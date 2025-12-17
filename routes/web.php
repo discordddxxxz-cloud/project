630a96e37aa9cc6fc27748ae01485022b1b2ac54
SetRose <discorddd.xxxz@gmail.com>
2025-12-17 21:43:45 +0500

    9 задание

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,15 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MainController;
 use App\Http\Controllers\ReportController;
-use App\Http\Middleware\Admin;
-
 use App\Http\Controllers\AdminController;
-
+use App\Http\Middleware\Admin;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    return redirect()->route('reports.index');
+    return Auth::check()
+        ? redirect()->route('reports.index')
+        : redirect()->route('login');
 });
 
 Route::middleware('auth')->group(function () {
@@ -18,20 +17,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/reports/create', [ReportController::class, 'create'])->name('reports.create');
     Route::post('/reports', [ReportController::class, 'store'])->name('reports.store');
     Route::get('/reports/{report}', [ReportController::class, 'show'])->name('reports.show');
-    Route::get('/reports/{report}/edit', [ReportController::class, 'edit'])->name('reports.edit');
     Route::put('/reports/{report}', [ReportController::class, 'update'])->name('reports.update');
     Route::delete('/reports/{report}', [ReportController::class, 'destroy'])->name('reports.destroy');
 });
 
-require __DIR__ . '/auth.php';
-
 Route::middleware(Admin::class)->group(function () {
-    Route::get('/admin', [AdminController::class, 'index'])
-        ->name('admin.index');
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
 
-    Route::patch(
-        '/reports/status/{report}',
-        [ReportController::class, 'statusUpdate']
-    )->name('reports.status.update');
+    Route::patch('/reports/status/{report}', [ReportController::class, 'statusUpdate'])
+        ->name('reports.status.update');
 });
 
+require __DIR__ . '/auth.php';
